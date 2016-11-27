1b551abc2002a8aaa82b26268ad649348c4ba6ef
daniel <dgr7348@students.fhv.at>
2016-11-27 08:49:31 +0100

    msp430 + jflap ue04

---
+++ Semester 1/C/MSP430/Blink LED/Debug/blink.d
@@ -0,0 +1,15 @@
+# FIXED
+
+blink.obj: ../blink.c
+blink.obj: /Applications/ti/ccsv6/ccs_base/msp430/include/msp430.h
+blink.obj: /Applications/ti/ccsv6/ccs_base/msp430/include/msp430fr6989.h
+blink.obj: /Applications/ti/ccsv6/ccs_base/msp430/include/in430.h
+blink.obj: /Applications/ti/ccsv6/tools/compiler/msp430_15.12.3.LTS/include/intrinsics.h
+blink.obj: /Applications/ti/ccsv6/tools/compiler/msp430_15.12.3.LTS/include/intrinsics_legacy_undefs.h
+
+../blink.c: 
+/Applications/ti/ccsv6/ccs_base/msp430/include/msp430.h: 
+/Applications/ti/ccsv6/ccs_base/msp430/include/msp430fr6989.h: 
+/Applications/ti/ccsv6/ccs_base/msp430/include/in430.h: 
+/Applications/ti/ccsv6/tools/compiler/msp430_15.12.3.LTS/include/intrinsics.h: 
+/Applications/ti/ccsv6/tools/compiler/msp430_15.12.3.LTS/include/intrinsics_legacy_undefs.h: 
